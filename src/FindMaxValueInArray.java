19de097cfd4e481eb393dd707a4c40780d08c7fc
Tien Dat <datdta95@gmail.com>
2018-08-07 10:16:19 +0700

    Khai báo mảng Gán kích thước của mảng bằng size Nhập giá trị của các phần tử trong mảng

--- src/FindMaxValueInArray.java
+++ src/FindMaxValueInArray.java
@@ -11,5 +11,10 @@ public class FindMaxValueInArray {
                 System.out.println("Kích thước tối đa của mảng là 20");
             }
         } while (size > 20);
+        int[] money = new int[size];
+        for (int i = 0; i < size; i++) {
+            System.out.println("Nhập vào số tiền của tỷ phú thứ " + (i + 1));
+            money[i] = scanner.nextInt();
+        }
     }
 }
